2cbfb467e649094c17a6dcd2dffdc33c4e5516b8
RobertSShoemaker <shoemakerrs@att.net>
2021-09-25 18:58:14 -0500

    -Shooter defender will now transition into attack or idle animation depending on if an attacker is in the same lane.

--- Assets/Scripts/AttackerSpawner.cs
+++ Assets/Scripts/AttackerSpawner.cs
@@ -23,9 +23,11 @@ public class AttackerSpawner : MonoBehaviour
         }
     }
 
+    //instantiate the attacker
     private void SpawnAttacker()
     {
-        Instantiate(attackerPrefab, transform.position, transform.rotation);
+        Attacker newAttacker = Instantiate(attackerPrefab, transform.position, transform.rotation) as Attacker;
+        newAttacker.transform.parent = transform;
     }
 
 
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,6 +6,68 @@ public class Shooter : MonoBehaviour
 {
 
     [SerializeField] GameObject projectile, gun;
+    AttackerSpawner myLaneSpawner;
+    Animator animator;
+
+
+    private void Start()
+    {
+        SetLaneSpawner();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //if an attacker is in the lane then start the attack animation, else start the idle animation
+        if (IsAttackerInLane())
+        {
+            //isAttacking was setup on the animation controller transitions
+            animator.SetBool("isAttacking", true);
+
+        } 
+        else
+        {
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+
+    //determines the lane of an attacker's spawner
+    //only need to determine this once, since the other attacker's from this lane will have the same location
+    private void SetLaneSpawner()
+    {
+        //array of all attack spawners
+        AttackerSpawner[] attackerSpawners = FindObjectsOfType<AttackerSpawner>();
+
+        //find this lane's corresponding spawner
+        foreach(AttackerSpawner attackerSpawner in attackerSpawners)
+        {
+            //determines whether the attacker's spawner and defender(shooter) are in the same lane
+            //finds the difference of the attacker's spawner and defender's y positions; if it's less than or equal to epsilon then they are in the same lane
+            //epsilon is the smallest float value bigger than 0
+            //need absolute value since the y values can be negative
+            bool IsCloseEnough = (Mathf.Abs(attackerSpawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            if (IsCloseEnough)
+            {
+                //only one spawner per lane
+                myLaneSpawner = attackerSpawner;
+            }
+        }
+    }
+
+    //determine if an attacker is in the same lane as the defender
+    private bool IsAttackerInLane()
+    {
+        //if there are no attacker children in the spawner for this lane, then return false
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 
     //shoots projectile from the position of the gun
     public void Fire()
