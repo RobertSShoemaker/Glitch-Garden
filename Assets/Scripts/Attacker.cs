e0d86b42a0273dbfb1cdb274e287836c9e143ce7
RobertSShoemaker <shoemakerrs@att.net>
2021-09-28 21:41:24 -0500

    -Lizards now damage defenders during attack animation -added health to defenders

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,16 +14,40 @@ public class Attacker : MonoBehaviour
     {
         //make enemy move from right to left
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+        UpdateAnimationState();
+
     }
 
+    private void UpdateAnimationState()
+    {
+        if(!currentTarget)
+        {
+            GetComponent<Animator>().SetBool("isAttacking", false);
+        }
+    }
+
+
+    //set movement speed
     public void SetMovementSpeed(float speed)
     {
         currentSpeed = speed;
     }
 
+    //changes animation to attacking and sets the currentTarget to the game object it's attacking
     public void Attack(GameObject target)
     {
         GetComponent<Animator>().SetBool("isAttacking", true);
         currentTarget = target;
+    }
+
+    public void StrikeCurrentTarget(float damage)
+    {
+        if (!currentTarget) { return; }
+        Health health = currentTarget.GetComponent<Health>();
+        if (health)
+        {
+            health.DealDamage(damage);
+        }
+
     }
 }
