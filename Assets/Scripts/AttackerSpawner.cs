49fd0cf28ee1f11668ab8175a01ad9d02ce92aa2
RobertSShoemaker <shoemakerrs@att.net>
2021-10-01 18:34:47 -0500

    -Spawners now spawn a random enemy

--- Assets/Scripts/AttackerSpawner.cs
+++ Assets/Scripts/AttackerSpawner.cs
@@ -7,7 +7,7 @@ public class AttackerSpawner : MonoBehaviour
 {
     [SerializeField] float minSpawnDelay = 1f;
     [SerializeField] float maxSpawnDelay = 5f;
-    [SerializeField] Attacker attackerPrefab;
+    [SerializeField] Attacker[] attackerPrefabArray;
 
     Attacker attacker;
     bool spawn = true;
@@ -26,18 +26,15 @@ public class AttackerSpawner : MonoBehaviour
     //instantiate the attacker
     private void SpawnAttacker()
     {
-        Attacker newAttacker = Instantiate(attackerPrefab, transform.position, transform.rotation) as Attacker;
+        int randomAttacker = UnityEngine.Random.Range(0, attackerPrefabArray.Length);
+        Spawn(attackerPrefabArray[randomAttacker]);
+    }
+
+    private void Spawn(Attacker myAttacker)
+    {
+        Attacker newAttacker = Instantiate(myAttacker, transform.position, transform.rotation) as Attacker;
         newAttacker.transform.parent = transform;
     }
-
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
 
    
 }
