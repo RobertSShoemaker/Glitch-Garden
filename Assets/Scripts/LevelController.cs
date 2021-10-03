8c3e4dcbbe8035fc6ca9c42c79dc4f9c82063f21
RobertSShoemaker <shoemakerrs@att.net>
2021-10-03 17:35:18 -0500

    -Added a win message and sound when player beats the level, the next level will also load after beating the level

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -5,21 +5,42 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
+    [SerializeField] GameObject winLabel;
+    [SerializeField] int winDelay = 5;
     int numberOfAttackers = 0;
     bool levelTimerFinished = false;
 
+    
+    private void Start()
+    {
+        //stops scene from displaying the win message at the start of the game
+        winLabel.SetActive(false);
+    }
+
+    //keeps track of the number of Attackers on screen; add one when an attacker spawns
     public void AttackerSpawned()
     {
         numberOfAttackers++;
     }
 
+    //keeps track of the number of Attackers on screen; subtract one when an attacker is killed
+    //win condition is met when there are no more attackers and the level timer is finished
     public void AttackerKilled()
     {
         numberOfAttackers--;
         if(numberOfAttackers <= 0 &&  levelTimerFinished)
         {
-            Debug.Log("End level Now!");
+            StartCoroutine(HandleWinCondition());
         }
+    }
+
+    //play win audio, display win message, then load the next level after a short delay
+    IEnumerator HandleWinCondition()
+    {
+        winLabel.SetActive(true);
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(winDelay);
+        GetComponent<LevelLoader>().LoadNextScene();
     }
 
     //stops spawning attackers when the time runs out; called by GameTimer
